43df0584ec06e073967b0afc1e7de7b5e08596ce
Ross Rosenlof <ross.rosenlof@fedex.com>
2019-07-30 13:27:24 -0600

    got the icons working, changing some content

---
+++ src/pages/hobbies.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+import SiteLinks from "../components/siteLinks"
+
+const HobbiesPage = () => (
+  <Layout>
+    <h1>Hobbies</h1>
+    <p>Here are some of hobbies:</p>
+    <p>List of blog .md posts</p>
+    <SiteLinks></SiteLinks>
+  </Layout>
+)
+
+export default HobbiesPage
--- src/pages/school.js
+++ src/pages/school.js
@@ -8,7 +8,7 @@ const SchoolPage = () => (
     <p>Here's a quick summary of the schooling that I've done since high school:</p>
     <div class="work-section">
       <h3>IS Program <span role="img">&#128218;</span></h3>
-      <p>In 2014, I started attending <a href="https://www.byu.edu/">Brigham Young University</a> in Provo, Utah. After a two year pause and some searching, I landed upon the <a href="https://marriottschool.byu.edu/infosys/">Information Systems</a> program in the Marriott School of Business. Why?</p>
+      <p>In 2014, I started attending <a href="https://www.byu.edu/">Brigham Young University</a> in Provo, Utah. After a two year pause and some searching, I landed upon the <a href="https://marriottschool.byu.edu/infosys/">Information Systems</a> program in the Marriott School of Business. I loved the combination technical and business classes offered, since I was (and still am!) uncertain of my future career path.</p>
       <p>I started the program in the fall of 2018. Over that school year, I took classes covering the following subjects:</p>   
       <ul>
         <li>C# ASP.Net programming</li>
@@ -16,13 +16,14 @@ const SchoolPage = () => (
         <li>Project management</li>
         <li>Django Python programming</li>
         <li>Machine learning in Azure ML Studio</li>
-        <li>IT Networking</li>
+        <li>IT networking</li>
         <li>Cybersecurity</li>
       </ul>
+      <p>It was a difficult two semesters, but I got a lot of help from great team members and professors.</p>
     </div>  
     <div class="work-section">  
       <h3>Masters program &#127891;</h3>
-      <p>I applied to and was accepted into the <a href="https://marriottschool.byu.edu/infosys/mism/what-will-i-study/program-overview/">Masters of Information Systems Management</a> program, an 2 year integrated graduate program. I will graduate in April 2021 with my bachelors of science in Information Systems and a masters of Information Systems Management. This coming fall, I will take classes about:</p>
+      <p>I applied to and was accepted into the <a href="https://marriottschool.byu.edu/infosys/mism/what-will-i-study/program-overview/">Masters of Information Systems Management</a> program, an 2 year integrated graduate program at BYU. I will graduate in April 2021 with my bachelor's of science in Information Systems and a master's degree in Information Systems Management. This coming fall, I will take classes about:</p>
       <ul>
         <li>Data analytics</li>
         <li>Enterprise infrastructure</li>
