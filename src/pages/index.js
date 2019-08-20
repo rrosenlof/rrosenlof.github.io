56d76b79cb5b638ee68bc7c9c2530dc9430995be
rrosenlof <rosenlof.ross@gmail.com>
2019-08-20 01:14:15 -0600

    adding some style changes for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,26 +10,26 @@ export default (props) => {
   <Layout>
     <h1>Hello!</h1>
     <p>I'm a student in the Masters of Information Systems Management program at Brigham Young University in Provo, Utah. Look around at some things that I've worked on and that I am interested in!</p>
-    <div class="row">
+    {/* <div class="row"> */}
       <Link to="/work/">
         <div class="card-link">
-          <h4>Work</h4>
-          <p>Read about some work experience that I have had</p>
+          <h4>Work:</h4> 
+          <p>Read about some of my work experience</p>
         </div>
       </Link>
       <Link to="/school/">
         <div class="card-link">
-          <h4>School</h4>
-          <p>Read about my coursework at BYU</p>
+          <h4>School:</h4>
+          <p>Check out what I do in classes at BYU</p>
         </div>
       </Link>
       <Link to="/projects/">
         <div class="card-link">
-          <h4>Projects</h4>
+          <h4>Projects:</h4>
           <p>See some projects that I have worked on</p>
         </div>
       </Link>
-    </div>
+    {/* </div> */}
     {/* <div class="row">
       
       <Link to="/hobbies/">
