255801aa61052b84e6aa330e4fda6fd8396fea51
ross <rosenlof.ross@gmail.com>
2020-11-03 22:40:28 -0700

    small update to school

--- src/pages/school.js
+++ src/pages/school.js
@@ -30,6 +30,8 @@ const SchoolPage = () => (
       </ul>
       <p>I am also completing a capstone project. With a group of 3 students, I will be developing a mobile application to help track seizures in people with epilepsy, and reveal trends about seizures over time. The application will probably be written in React Native, with an external API for processing data from an Apple Watch. Excited to work more on this project my last semester of the program! </p>
       <p>I've enjoyed diving deeper into subjects that I've studied before. Over the next two semesters, I will follow the Development track in the program. School is fun when your classes are interesting!</p>
+      </div>
+      <div class="work-section">
       <h3>IS Program <span role="img">&#128218;</span></h3>
       <p>In 2014, I started attending <a href="https://www.byu.edu/">Brigham Young University</a> in Provo, Utah. After a two year pause and some searching, I landed upon the <a href="https://marriottschool.byu.edu/infosys/">Information Systems</a> program in the Marriott School of Business. I loved the combination of technical and business classes offered, since I was (and still am!) uncertain of my future career path.</p>
       <p>I started the program in the fall of 2018. Over that school year, I took classes covering the following subjects:</p>   
