ee9c9d2558d4943c35279752fd5806e4096526bf
ross <rosenlof.ross@gmail.com>
2020-11-03 22:35:00 -0700

    adding gigup, r2n, updating school page

--- src/pages/school.js
+++ src/pages/school.js
@@ -6,27 +6,33 @@ const SchoolPage = () => (
   <Layout>
     <h1>School</h1>
     <p>Here's a quick summary of the schooling that I've done since high school:</p>
-    <div class="work-section">  
+    <div class="work-section">
       <h3>Masters of Information Systems Management Program &#127891;</h3>
-      <p>I applied to and was accepted into the <a href="https://marriottschool.byu.edu/infosys/mism/what-will-i-study/program-overview/">Masters of Information Systems Management</a> program, an 2 year integrated graduate program at BYU. I will graduate in April 2021 with my bachelor's of science in Information Systems and a master's degree in Information Systems Management. In fall of 2019, I took classes about:</p>
+      <p>I applied to and was accepted into the <a href="https://marriottschool.byu.edu/infosys/mism/what-will-i-study/program-overview/">Masters of Information Systems Management</a> program, an 2 year integrated graduate program at BYU. I will graduate in April 2021 with my bachelor's of science in Information Systems and a master's degree in Information Systems Management. I am currently or will take classes on:</p>
+      <ul class="work-section-list">
+        <li>Web Development</li>
+        <li>Data Analytics in Sports</li>
+        <li>VBA Programming</li>
+        <li>Global Supply Chain Management</li>
+        <li>UX Design</li>
+      </ul>
+      <p>In past semesters, I took classes about:</p>
       <ul class="work-section-list">
         <li>Data analytics (Python, SAS JMP)</li>
         <li>Enterprise infrastructure (AWS Products)</li>
         <li>Mobile app development (Swift for iOS apps)</li>
         <li>Cybersecurity</li>
-      </ul>
-      <p>And in the winter of 2020, I am taking classes about:</p>
-      <ul class="work-section-list">
         <li>Advanced Database Management (Oracle, Cassandra tools)</li>
         <li>Data Structures</li>
         <li>Business Strategy</li>
       </ul>
+      <p>I am also completing a capstone project. With a group of 3 students, I will be developing a mobile application to help track seizures in people with epilepsy, and reveal trends about seizures over time. The application will probably be written in React Native, with an external API for processing data from an Apple Watch. Excited to work more on this project my last semester of the program! </p>
       <p>I've enjoyed diving deeper into subjects that I've studied before. Over the next two semesters, I will follow the Development track in the program. School is fun when your classes are interesting!</p>
     </div>
     <div class="work-section">
       <h3>IS Program <span role="img">&#128218;</span></h3>
       <p>In 2014, I started attending <a href="https://www.byu.edu/">Brigham Young University</a> in Provo, Utah. After a two year pause and some searching, I landed upon the <a href="https://marriottschool.byu.edu/infosys/">Information Systems</a> program in the Marriott School of Business. I loved the combination of technical and business classes offered, since I was (and still am!) uncertain of my future career path.</p>
-      <p>I started the program in the fall of 2018. Over that school year, I took classes covering the following subjects:</p>   
+      <p>I started the program in the fall of 2018. Over that school year, I took classes covering the following subjects:</p>
       <ul class="work-section-list">
         <li>C# ASP.Net programming</li>
         <li>SQL and database design</li>
@@ -37,8 +43,8 @@ const SchoolPage = () => (
         <li>Cybersecurity</li>
       </ul>
       <p>It was a difficult two semesters, but I got a lot of help from great team members and professors.</p>
-    </div>  
-    
+    </div>
+
     <SiteLinks></SiteLinks>
   </Layout>
 )
---
+++ src/posts/GigUp/index.md
@@ -0,0 +1,34 @@
+---
+title: "Gig Up"
+date: "2020-02-20"
+category: "projects"
+description: "Flutter app for the 2020 BYU Mobile App Competition"
+---
+In winter of 2020, I worked with a group of three other MISM students to create a mobile app for Brigham Young University's Mobile App Competition..
+​
+## Background
+Every year, BYU's [Rollins Center](https://marriottschool.byu.edu/cet/) puts on a Mobile App Competition. The rules are pretty wide open and lots of cool apps are submitted to the contest. I formed a group with some fellow students a little less than a month before the deadline. We started working on a project called "Gig Up", an app for freelance workers to bid on projects posted by users in their communities.
+​
+Similar applications exist, like Thumbtack or Upwork, but our business model was different by putting more control in the hands of the users who could approve a contractor based on multiple received bids.
+​
+## Process
+All of my group had taken a mobile application development class together, where we learned Swift. However, we wanted to try something new and used [Flutter](https://flutter.dev/) for the project. One of my group members had made a simple app with Flutter before, but the rest of us had no experience. It was a bit difficult learning this new tool for developing in a short amount of time, especially since Flutter is relatively new. But Flutter's docs are great, and it is a pretty intuitive programming tool.
+​
+In addition to Flutter, we used [Firebase](https://firebase.google.com/) for our backend services, which integrates easily with Flutter – for obvious reasons! This served as our database, and provided user authentication and location service tools.
+​
+We mocked up the application and divided out the parts of the app in the 2 and a half weeks until the deadline. I was in charge of user authentication primarily. In our finished product, users would create an account or log in to an existing account to be able to post or bid on freelance projects. With Firebase and some helpful tutorials, I was able to get this functionality working before the deadline and help out my team on a few other aspects of the app as well.
+​
+## Finished Product
+​
+At the deadline of the App Competition, our app allowed users to authentice, post and bid on projects, and accept a winning bid for their project. We also had base functionality to view locations of posted projects on map view, similar to viewing AirBnb rentals in a given area.
+​
+We certainly could have added more to the project, given more time, but overall we achieved the functionality we wanted to present about the app for the App Competiton.
+​
+Since the competition, we have not worked more on project, we have not put the app on any app stores, and don't have plans to... which is fine with us! It was still a fun experience to work on for a couple of weeks.
+​
+## Takeaway
+While we did not place high in the App Competition, my team was still happy with our results for the amount of time we put into the project. It was stressful working with a short deadline, but we put in the work necessary to be able to present at the App Competition final and share the functionality of the app. 
+​
+Learning the basics of Flutter for this project was a good experience too. If I were to work on another mobile app, I would highly consider Flutter due to cross-platform capabilities, UI-based development, and compatability with Firebase. 
+​
+[Here](https://docs.google.com/presentation/d/1DwP21Fe0kw93DTJDWrrlYRC6sBM0FDzDjTFPwF8d9zI/edit?usp=sharing) are the slides we used to present this project, including screen captures of the app. 
